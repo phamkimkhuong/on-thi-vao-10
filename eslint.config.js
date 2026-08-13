ff06aacf1a6de2225d1f4d591f19bd8f46b896a5
PHAM KIM KHUONG <phamkhuong436@gmail.com>
2026-08-13 23:26:31 +0700

    update

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,9 @@ export default defineConfig([
       '@typescript-eslint/no-unused-vars': 'warn',
       'react-hooks/exhaustive-deps': 'warn',
       'react-hooks/set-state-in-effect': 'off',
+      'react-hooks/purity': 'off',
+      'react-hooks/preserve-manual-memoization': 'off',
+      'preserve-caught-error': 'off',
       'no-useless-escape': 'off',
     }
   },
